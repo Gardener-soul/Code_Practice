63555b8891dbac92f438237aa879916e06ddddbd
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-07-30 12:43:22 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 13136 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		List<Integer> arr = new ArrayList<>();
+		int cnt = 0;
+		
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(a<0) {
+				break;
+			}
+			if(b>10) {
+				break;
+			}
+			int sum = a+b;
+			arr.add(sum);
+			cnt++;
+		}
+		sc.close();
+		for(int i=0; i<cnt; i++) {
+			System.out.println(arr.get(i));
+	}
+}
+}
