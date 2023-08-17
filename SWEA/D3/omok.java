46507566fae88bed336f08c286144769f2753fb0
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-08-17 15:14:34 +0900

    Update omok.java

--- SWEA/D3/omok.java
+++ SWEA/D3/omok.java
@@ -1,53 +1,56 @@
-package omok;
+package Omok;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-        for(int tc=1; tc<=T; tc++) {
-		int N = sc.nextInt();
-		char[][] omok = new char[N][N];  ///
-		for (int i = 0; i < N; i++) {
-			String str = sc.next();
-			for (int j = 0; j < N; j++) {
-				omok[i][j] = str.charAt(j);
-			}
-		} // 오목판 생성 완료
-        
-		int[] dx= {-1,1,0,0,-1,1,-1,1}; //
-        int[] dy= {0,0,-1,1,-1,-1,1,1}; //
-        
-		boolean flag=false; //
-		f1: for(int i=0; i<N; i++) { //
-			for(int j=0; j<N; j++) {
-				if(omok[i][j]=='o') {
-					for(int d=0; d<8; d++) {
-						int len=1;
-						int si=i;
-						int sj=j;
-						while(true) {
-							int nx = si + dx[d];
-							int ny = sj + dy[d];
+		
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();//테스트 케이스 개수
+		for(int testCase = 1; testCase <= T; testCase++) {
+			int N = scanner.nextInt(); // 오목판 크기
+			char[][] map = new char [N][N]; 
+			for(int i=0; i<N; i++) {
+				String str = scanner.next(); //한줄씩 받고
+				for(int j=0; j<N; j++) {
+					map[i][j] = str.charAt(j); //그 줄의 문자 인덱스만큼 분류해서 넣기
+				}
+			} //omok 잘 받아짐
+			
+			//delta 만들기 우 , 하 ,대우하, 대좌하
+			int[] dr = {0,1,1,1};
+			int[] dc = {1,0,1,-1};
+			
+			String ans = "NO"; //오목이 만들어지면 Yes로 바꿀거임.
+			
+			out : for(int r=0; r<N; r++) {
+				for (int c=0; c<N; c++) {
+					if(map[r][c]=='o') { //만약 o를 만났을 때 시작한다.
+						
+						for(int d = 0; d<4; d++) { //델타값을 반복문으로 주고 그 안에서 (ex cnt값 주기)를 통해 설정한 delta 방향으로 순회 가능
 							
-							if(nx>=0 && nx<N && ny>=0 && ny<N&&omok[nx][ny]=='o') {
-								si=nx;
-								sj=ny;
-								len++;
-								
-							}else break;
-							if(len==5) {
-								flag=true;
-								break f1;
+							int cnt = 1;
+							int nr = r + dr[d]; //nr과 nc 는 d값이 변경될때마다 다시 초기화가 되어야함.
+							int nc = c + dc[d];
+							
+							while(nr>=0&&nc>=0&&nr<N&&nc<N&&map[nr][nc]=='o') { //nr nc 위치에서도 o가 나왔다면 이제 본격적으로 확인해보기 시작.
+								nr+=dr[d]; // 현재 순회하고 있는 d 반복문 내에서 계속해서 확인할거임.
+								nc+=dc[d];
+								cnt++;
 							}
+							
+							if(cnt==5) {
+								ans = "YES";
+								break out; //오목이 한 번 완성되면 굳이 더 확인할 필요가 없기 때문에 반복문을 아예 탈출
+							}
+							
 						}
 					}
 				}
 			}
-		}
-		if(flag) { System.out.println("#"+tc+" "+"YES");}
-			else {System.out.println("#"+tc+" "+"NO");}
-        }// tc반복문
+			System.out.println("#" + testCase + " " + ans);
+			
+		} //tc
 	}
 }
