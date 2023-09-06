bd193c5a74f85dea979e3c2a5d63c10ddc2446ad
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-09-06 16:01:50 +0900

    [Silver IV] Title: solved.ac, Time: 320 ms, Memory: 37400 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -3,21 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-class Main {
-	public static void main(String[] args) throws IOException{
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine()); //난이도 의견의 개수
 		int[] arr = new int[n];
-
-		for(int i=0; i<n; i++)
+		
+		for(int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-
-		// 오름차순 정렬
-		Arrays.sort(arr);
-		int num = (int)Math.round(n*0.15);
+		}
+		Arrays.sort(arr); //크기순 정렬
+		
+		int cnt = (int) Math.round(n*0.15);
+		
 		double sum = 0;
-		for(int i=num; i<n-num; i++)
-			sum += arr[i];
-		System.out.println((int)Math.round(sum/(n-num*2)));
-	}
+		for(int i=cnt; i<n-cnt; i++) {
+			sum+=arr[i];
+		}
+		
+		System.out.println((int) Math.round(sum / (n-cnt*2)));
+	} //main
 }
