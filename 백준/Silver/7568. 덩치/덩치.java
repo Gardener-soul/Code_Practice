7af75f12abb1f02bfe1728a3152cd071ac8a9260
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-09-12 17:19:52 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 12972 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,26 +1,32 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		int[][] dung = new int[N][2];
+		StringBuilder sb = new StringBuilder();
+		int N = scanner.nextInt(); // 사람의 배열.
+		int[][] arr = new int[N][N];
+		
 		for(int i=0; i<N; i++) {
-			dung[i][0] = scanner.nextInt();
-			dung[i][1] = scanner.nextInt();
+			arr[i][0] = scanner.nextInt();
+			arr[i][1] = scanner.nextInt();
 		}
+		
+		
 		for(int i=0; i<N; i++) {
 			int rank = 1;
+			
 			for(int j=0; j<N; j++) {
-				if(i==j) continue;
-				if(dung[i][0]<dung[j][0] && dung[i][1]<dung[j][1]) {
+				
+				if(i==j) continue;			
+				if(arr[i][0]<arr[j][0] && arr[i][1]<arr[j][1]) {
 					rank++;
 				}
-				
+	
 			}
-			System.out.print(rank + " ");
+			sb.append(rank + " ");
 		}
+		System.out.println(sb);
 	}
 }
