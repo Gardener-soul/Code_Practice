4e14894007eb7905df1905d9d97ac47056d2c2b2
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-09-18 22:01:52 +0900

    [unrated] Title: 글자 지우기, Time: 2.40 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -2,10 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int[] indices) {
+        String answer = "";
         Arrays.sort(indices);
         
-        for(int i = 0; i < indices.length; i ++) my_string = my_string.substring(0, indices[i] - i) + my_string.substring(indices[i] - i + 1);
+        List<String> ans = new ArrayList<>();
+		
+		String[] str = my_string.split("");
+		for(int i=0; i<indices.length; i++) {			
+			str[indices[i]] = "";
+		}
+		
+		for(int i=0; i<str.length; i++) {
+			if(!str[i].equals("")) {
+				ans.add(str[i]);
+			}
+		}
+		for(int i=0; i<ans.size(); i++) {
+			answer+=ans.get(i);
+		}
         
-        return my_string;
+        return answer;
     }
 }
