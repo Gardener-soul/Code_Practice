a964e0ef60e81a9337ca034232970dade18f3b41
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-09-22 12:08:00 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 152 ms, Memory: 20,660 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -4,70 +4,90 @@ import java.util.Scanner;
 
 class Pair{
 	
-	int x;
-	int y;
-	
+	int x, y;
+
 	public Pair(int x, int y) {
 		super();
 		this.x = x;
 		this.y = y;
 	}
+	
 }
 
 public class Solution {
-
-	static char[][] map;
+	
 	static int[] dr = {-1,1,0,0};
 	static int[] dc = {0,0,-1,1};
+	static char[][] map;
+	static boolean[][] chk;
 	static Queue<Pair> q;
 	
 	public static void bfs(int x, int y) {
 		
+		chk[x][y] = true;
+		map[x][y] = '1';
 		q.offer(new Pair(x,y));
-		map[x][y]='1';
 		
 		while(!q.isEmpty()) {
 			
-			Pair t = q.poll();
-			
+			Pair tmp = q.poll();
 			for(int d=0; d<4; d++) {
 				
-				int tr = t.x + dr[d];
-				int tc = t.y + dc[d];
+				int tr = tmp.x + dr[d];
+				int tc = tmp.y + dc[d];
 				
-				if(map[tr][tc]=='0') {
-					q.offer(new Pair(tr,tc));
-					map[tr][tc] = '1';
-				}
-				
-				if(map[tr][tc]=='3') {
-					System.out.println(1);
-					return;
+				if(tr<16 && tc<16 && tr>=0 && tc>=0) {
+					
+					if(!chk[tr][tc] && map[tr][tc] == '0' || map[tr][tc] == '3') {
+						
+						chk[tr][tc] = true;
+						map[tr][tc] = '1';
+						q.offer(new Pair(tr, tc));
+						
+					}
+					
 				}
 				
 			}
 			
 		}
 		
-		System.out.println(0);
-		
 	}
 	
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		for(int testCase = 1; testCase<=10; testCase++) {
+		for(int testCase = 1; testCase <= 10; testCase++) {
 			
-			int T = scanner.nextInt();
+			int T = scanner.nextInt(); // 의미없는 숫자 값.
+			
 			map = new char[16][16];
-			q=new LinkedList<>();
+			chk = new boolean[16][16];
+			q = new LinkedList<>();
 			
-			for(int r=0; r<16; r++) {
-				map[r] = scanner.next().toCharArray();
+			for(int i=0; i<16; i++) {
+				map[i] = scanner.next().toCharArray();
 			}
 			
-			System.out.print("#" + testCase + " ");
+			int r = 0;
+			int c = 0;
+			
+			outer : for(r = 0; r<16; r++) {
+				for (c=0; c<16; c++) {
+					if(map[r][c]=='3') {
+						break outer;
+					}
+				}
+			}
+			
 			bfs(1,1);
+			
+			int ans = 0;
+			if(chk[r][c]) {
+				ans=1;
+			}
+			
+			System.out.println("#" + testCase + " " + ans);
 			
 		}
 		
