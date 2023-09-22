cf7ff5d88e6596b96ea83fd381ed2f76a0ef95d5
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-09-22 12:44:34 +0900

    [D3] Title: 최장 경로, Time: 156 ms, Memory: 22,232 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.java
+++ SWEA/D3/2814. 최장 경로/최장 경로.java
@@ -1,71 +1,68 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
 	
-	static int N, M, answer;
-	static ArrayList<Integer>[] graph;
-	static int[] chk;
+	static int N;
+	static ArrayList<Integer>[] map;
+	static boolean[] chk;
+	static int max;
 	
-	public static void dfs(int level, int Node) {
+	public static void dfs(int level , int cnt) {
 		
-		if(level == N) {
-			answer = N;
-		} else {
+
+		for(int i=0; i<map[level].size(); i++) {
 			
-			for(int nextNode : graph[Node]) {
-				
-				if(chk[nextNode]==0) {
-					
-					chk[nextNode] = 1;
-					dfs(level+1, nextNode);
-					chk[nextNode] = 0;
-					
-				}
+			int nextLevel = map[level].get(i);
+			
+			if(!chk[nextLevel]) {
+				chk[nextLevel] = true;
+				dfs(nextLevel, cnt+1);
+				chk[nextLevel] = false;
 			}
 			
-			answer = Math.max(level, answer);
-			
 		}
+		
+		max = Math.max(max, cnt);
+//		System.out.println(max);
+		
 	}
 	
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		int T = scanner.nextInt();
-		
-		for(int testCase = 1; testCase<=T; testCase++) {
+		for(int testCase=1; testCase<=T; testCase++) {
 			
-			N = scanner.nextInt(); // 정점의 개수
-			M = scanner.nextInt(); // 간선의 개수
-			answer = -1;
-			
-			graph = new ArrayList[N+1];
-			chk = new int[N+1];
+			N = scanner.nextInt(); // 정점
+			int M = scanner.nextInt(); // 간선
+			map = new ArrayList[N+1];
+			chk = new boolean[N+1];
+			max = 0;
 			
 			for(int i=1; i<=N; i++) {
-				graph[i] = new ArrayList<>();
+				map[i] = new ArrayList<>();
 			}
 			
 			for(int i=0; i<M; i++) {
+				
 				int a = scanner.nextInt();
 				int b = scanner.nextInt();
 				
-				graph[a].add(b);
-				graph[b].add(a);
-			} // 그래프 생성 완료
-			
-			for(int i=1; i<=N; i++) {
-				
-				chk[i] = 1;
-				dfs(1,i);
-				chk[i]=0;
+				map[a].add(b);
+				map[b].add(a);
 				
 			}
 			
-			System.out.println("#" + testCase + " " + answer);
+			for(int i=1; i<=N; i++) {
+				chk[i] = true;
+				dfs(i,1);
+				chk[i] = false;
+			}
+			
+			System.out.println("#" + testCase + " " + max);
 			
 		} // tc
+		
 	}
 }
