02a79d5f2b8cb53924c69c1d7adccb4432a51401
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-09-26 08:56:41 +0900

    [level 2] Title: 타겟 넘버, Time: 1.08 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,38 @@
 class Solution {
     
-     public static void dfs(int[] numbers, int target, int idx, int sum){
-        
-        if(idx==numbers.length){
-            if(target==sum){
-                cnt++;
-            }   
+    static int sum;
+    static int answer;
+    
+    public static void tf(int[] numbers, boolean[] chk, int target, int idx){
+        sum=0;
+        if(idx == numbers.length){
             
-        } else {
+            for(int i=0; i<numbers.length; i++){
+                if(chk[i]){
+                    sum+=numbers[i];
+                } else {
+                    sum-=numbers[i];
+                }
+            }
             
-        dfs(numbers, target, idx + 1, sum+numbers[idx]);
-        dfs(numbers, target, idx + 1, sum-numbers[idx]);
-        
+            if(sum == target){
+                answer++;
+            }
+            
+            return;
         }
-    }    
+        
+        chk[idx] = false;
+        tf(numbers, chk, target, idx+1);
+        
+        chk[idx] = true;
+        tf(numbers, chk, target, idx+1);
+    }
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0,0);
-        int answer = cnt;
+        boolean[] chk = new boolean[numbers.length];
+        tf(numbers, chk, target, 0);
+        
         return answer;
-    } 
-    
-    static int cnt = 0;
+    }
 }
