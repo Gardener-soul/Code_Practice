0f3e99e40593bbe8f444fe90f2acbfe40cffca82
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-09-28 19:01:17 +0900

    [D3] Title: 햄버거 다이어트, Time: 783 ms, Memory: 24,452 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,67 +1,60 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
 	
-	static int N; // 음식 개수
-	static int L; // 최대 칼로리
-	static int[] kcal; //칼로리
-	static int[] score; //점수
-	static boolean[] chk; //chk배열
-	static int max;
+	static int N,L,ans;
+	static boolean[] chk;
+	static int[] score, kcal;
 	
-	public static void main(String[] args) {
+	public static void powerset(int idx) {
 		
-		Scanner scanner = new Scanner(System.in);
-		int T = scanner.nextInt();
-		for(int testCase = 1; testCase <= T; testCase++) {
+		if(idx == N) {
 			
-			max=-1;
-			N = scanner.nextInt();
-			L = scanner.nextInt();
-			kcal = new int[N];
+			int ksum = 0;
+			int ssum = 0;
+			
+			for(int i=0; i<N; i++) {
+				if(chk[i]) {
+					ksum+=kcal[i];	
+					ssum+=score[i];
+				}
+			}
+			
+			if(ksum<=L) {
+				ans = Math.max(ans, ssum);
+			}
+			return;
+		}
+
+		chk[idx] = false;
+		powerset(idx+1);
+		
+		chk[idx] = true;
+		powerset(idx+1);
+		
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int T = scanner.nextInt();
+		
+		for(int testCase = 1; testCase<=T; testCase++) {
+			N = scanner.nextInt(); // 재료 개수
+			L = scanner.nextInt(); // 허용되는 칼로리
+			ans=0;
 			score = new int[N];
+			kcal = new int[N];
 			chk = new boolean[N];
 			
 			for(int i=0; i<N; i++) {
 				score[i] = scanner.nextInt();
 				kcal[i] = scanner.nextInt();
 			}
-			
-			ksum(0);
-			
-			System.out.println("#" + testCase + " " + max);
-			
+			powerset(0);
+			System.out.println("#" + testCase + " " + ans);
 		}
-		
-	}
-	
-	static void ksum(int idx) {
-	
-		if(idx==N) {
-			int ksum = 0; // 칼로리 점수
-			int sc = 0; //스코어 점수
-			for(int i=0; i<N; i++) {
-				if(chk[i]) {
-					sc+=score[i];
-					ksum+=kcal[i];
-				}
-			}
-			
-			if(ksum<=L) {
-				max = Math.max(max, sc);
-			}
-			
-			return;
-		} 
-		
-		chk[idx] = false;
-		ksum(idx+1);
-		
-		chk[idx] = true;
-		ksum(idx+1);
 		
 	}
 	
