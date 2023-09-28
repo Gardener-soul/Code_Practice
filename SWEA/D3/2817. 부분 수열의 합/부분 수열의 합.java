1dedade0eb416bb5951baccc250e5b05c1651bec
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-09-28 23:14:57 +0900

    [D3] Title: 부분 수열의 합, Time: 1,202 ms, Memory: 23,696 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,61 +1,55 @@
 import java.util.Scanner;
 
 public class Solution {
-	
+	static int N,A, cnt;
 	static int[] num;
-	static int k;
 	static boolean[] chk;
-	static int cnt;
+	
+	public static void powerset(int idx) {
+		
+		int sum = 0;
+		
+		if(idx==N) {
+			
+			for(int i=0; i<N; i++) {
+				if(chk[i]) {
+					
+					sum+=num[i];
+					
+				}
+			}
+			
+			if(sum==A) cnt++;
+			
+			return;
+		}
+		
+		chk[idx] = false;
+		powerset(idx+1);
+		
+		chk[idx] = true;
+		powerset(idx+1);
+		
+	}
 	
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		int T = scanner.nextInt();
 		
-		for(int testCase = 1; testCase <= T; testCase++) {
-		
-			int N = scanner.nextInt();
-			int K = scanner.nextInt();
+		for(int testCase = 1; testCase<=T; testCase++) {
 			
+			N = scanner.nextInt();
+			A = scanner.nextInt(); // 나와야 하는 수
+			num = new int[N];
+			chk = new boolean[N];
 			cnt = 0;
-			num = new int [N];
-			k = K;
-			chk = new boolean [N];
 			
 			for(int i=0; i<N; i++) {
 				num[i] = scanner.nextInt();
 			}
-			
-			sum(0);
-			System.out.println("#" + testCase + " " + cnt);
-				
-			}
-			
+			powerset(0);
+			System.out.println("#" + testCase+" " + cnt);
 		}
-	
-	static void sum(int idx) {
-		
-		
-		if(idx==num.length) {			
-			int sum=0;
-			for(int i=0; i<num.length; i++) {
-				if(chk[i]) {
-					sum+=num[i];
-				}
-			}
-			
-			if(sum==k) {
-				cnt++;
-			}
-			
-			return;
-		}
-		
-		chk[idx] = false;
-		sum(idx+1);
-		
-		chk[idx] = true;
-		sum(idx+1);
-		
 	}
 }
