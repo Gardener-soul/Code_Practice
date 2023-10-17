1192208df1d4078d97dbfa85932077b409f50a12
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-10-18 08:00:23 +0900

    [Bronze II] Title: 분해합, Time: 104 ms, Memory: 12760 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,40 +1,39 @@
 import java.util.Scanner;
- 
+
 public class Main {
 	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
-    
-		// 자릿수의 길이를 알기위해 일단 문자열로 입력받는다.
-		String str_N = in.nextLine();
- 
-		// 해당 문자열의 길이 변수
-		int N_len = str_N.length();
- 
-		// 문자열을 정수(int)로 변환 
-		int N = Integer.parseInt(str_N);
-		int result = 0;
- 
+
+		Scanner scanner = new Scanner(System.in);
+		String N = scanner.next();
+		int num = Integer.parseInt(N);
+		int len = N.length();
+		int idx = 0;
 		
-		// i 는 가능한 최솟값인 N - 9 * N의 각 자릿수부터 시작 
-		for(int i = (N - (N_len * 9)); i < N; i++) {
-			int number = i;
-			int sum = 0;	// 각 자릿수 합 변수 
+		for(idx=num-len*9; idx<num; idx++) {
+		
+			int tmp = len;			
+			int tmpNum = idx;
+			int sum = tmpNum;
 			
-			while(number != 0) {
-				sum += number % 10;	// 각 자릿수 더하기
-				number /= 10;
+			while(tmp>0) {
+				
+				sum+=tmpNum%10;
+				tmpNum/=10;
+						
+				tmp--;
 			}
 			
-			// i 값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우) 
-			if(sum + i == N) {
-				result = i;
+			if(sum==num) {
 				break;
 			}
 			
 		}
- 
-		System.out.println(result);
-	}
- 
+		
+		if(idx!=num) {			
+			System.out.println(idx);
+		}else {
+			System.out.println(0);
+		}
+		
+	} //main문
 }
