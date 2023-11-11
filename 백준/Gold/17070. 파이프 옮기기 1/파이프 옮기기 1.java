f5d94b1dc4a5322db0301e50d61e9dd3a38938bf
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-11-11 23:16:02 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 268 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -9,10 +9,6 @@ public class Main {
 	static int[] dr = {0,1,1}; // 가로 , 세로, 대각선
 	static int[] dc = {1,1,0};
 	static int[][] map;
-	static boolean[][] chk;
-	static boolean[][] piChk = {{true,false,true},
-								{false,true,true},
-								{true,true,true}};
 	
 	static void dfs(int r, int c, int dir) {
 		// dir = 가로 0 대각선 1 세로 2  
@@ -36,48 +32,8 @@ public class Main {
 			}
 		}
 	}
-	
-	public static void search(int pipe , int r, int c) {
-		
-		if(r==N && c==N) {
-			ans++;
-			return;
-		}
-		
-		for(int d=0; d<3; d++) {
-			
-			if(!piChk[pipe][d]) continue;
-			
-			int tr = r + dr[d];
-			int tc = c + dc[d];
-			
-			if(tr<=N && tc<=N && !chk[tr][tc]) {
-				if(map[tr][tc]==1) continue;
-				
-				chk[tr][tc] = true;
-				if(d==2) {
-					
-					if(map[tr-1][tc]==1 || map[tr][tc-1]==1) return;
-					
-					chk[tr-1][tc] = true;
-					chk[tr][tc-1] = true;
-				}
-				
-				search(d,tr,tc);
-				
-				chk[tr][tc] = false;
-				if(d==2) {
-					chk[tr-1][tc] = false;
-					chk[tr][tc-1] = false;
-				}
-				
-			}
-			
-		}
-		
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -85,19 +41,13 @@ public class Main {
 		N = Integer.parseInt(br.readLine()); //(N-1,N-1)에 도달했을 때
 		ans = 0;
 		map = new int[N+1][N+1];
-		chk = new boolean[N+1][N+1];
 		
 		for(int r=1; r<=N; r++) {
 			st = new StringTokenizer(br.readLine());
 			for(int c=1; c<=N; c++) {
 				map[r][c] = Integer.parseInt(st.nextToken());
-				if(map[r][c]==1) chk[r][c] = true;
 			}
 		}
-		
-		
-		chk[1][1] = true; 
-		chk[1][2] = true;
 
 		dfs(1,2,0);
 		System.out.println(ans);
