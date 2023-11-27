702c9d608086d26e3ffa489f1a323e1782e32d1d
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-11-27 22:57:15 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 152 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
@@ -1,79 +1,107 @@
-import java.util.*;
- 
-public class Main {
- 
-    static int n, m, t;
-    static int[][] board;
-    static boolean[][][] visited;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        n = scan.nextInt();
-        m = scan.nextInt();
-        t = scan.nextInt();
-        
-        board = new int[n][m];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                board[i][j] = scan.nextInt();
-            }
-        }
- 
-        visited = new boolean[n][m][2];// 0은 그람이 없을때, 1은 그람이 있을때
-        int result = bfs(0, 0);
-        if(result == -1) System.out.println("Fail");
-        else System.out.println(result);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Pair{
+    int r,c,cnt;
+    boolean sword;
+
+    public Pair(int r, int c, int cnt, boolean sword) {
+        this.r = r;
+        this.c = c;
+        this.cnt = cnt;
+        this.sword = sword;
     }
- 
-    public static int bfs(int x, int y) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(x, y, 0, false));
-        visited[x][y][0] = true;
- 
-        while(!q.isEmpty()) {
-            Node current = q.poll();
- 
-            if(current.count > t) break;
-            if(current.x == n - 1 && current.y == m - 1) return current.count;
- 
-            for(int i = 0; i < 4; i++) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if(!current.isGram) { //그람 없음
-                        if(!visited[nx][ny][0] && board[nx][ny] == 0) {
-                            q.offer(new Node(nx, ny, current.count + 1, current.isGram));
-                            visited[nx][ny][0] = true;
-                        } else if(!visited[nx][ny][0] && board[nx][ny] == 2) {
-                            q.offer(new Node(nx, ny, current.count + 1, true));
-                            visited[nx][ny][0] = true;
+}
+
+public class Main {
+
+    static int R,C,time, ans;
+    static int[] dr = {0,1,0,-1};
+    static int[] dc = {1,0,-1,0};
+    static int[][] map;
+    static boolean[][][] chk;
+    static Queue<Pair> q;
+
+    public static void bfs(int r, int c){
+
+        while(!q.isEmpty()){
+
+            Pair tmp = q.poll();
+            int nr = tmp.r;
+            int nc = tmp.c;
+            int ncnt = tmp.cnt;
+            boolean nsword = tmp.sword;
+
+            if(ncnt>time){
+                System.out.println("Fail");
+                return;
+            }
+
+            if(nr == R-1 && nc == C-1){
+                if(ncnt>time) System.out.println("Fail");
+                else System.out.println(ncnt);
+                return;
+            }
+
+            for(int d=0; d<4; d++){
+                int tr = nr + dr[d];
+                int tc = nc + dc[d];
+
+                if(tr<R && tc<C && tr>=0 && tc>=0){
+                    if(!nsword){
+                        if(map[tr][tc]==0 && !chk[tr][tc][0]){
+                            q.offer(new Pair(tr,tc,ncnt+1,false));
+                            chk[tr][tc][0] = true;
                         }
-                    } else { //그람 있음
-                        if(!visited[nx][ny][1]) {
-                            q.offer(new Node(nx, ny, current.count + 1, current.isGram));
-                            visited[nx][ny][1] = true;
+                        else if (map[tr][tc]==2 && !chk[tr][tc][0]) {
+                            q.offer(new Pair(tr,tc,ncnt+1,true));
+                            chk[tr][tc][1] = true;
+                        }
+                    } else if(nsword) {
+                        if(!chk[tr][tc][1]){
+                            chk[tr][tc][1] = true;
+                            q.offer(new Pair(tr,tc,ncnt+1,true));
                         }
                     }
                 }
             }
         }
-        return -1;
+        System.out.println("Fail");
     }
- 
-    public static class Node {
-        int x;
-        int y;
-        int count;
-        boolean isGram;
- 
-        public Node(int x, int y, int count, boolean isGram) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-            this.isGram = isGram;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        time = Integer.parseInt(st.nextToken());
+        ans = 0;
+
+        map = new int[R][C];
+        chk = new boolean[R][C][2];
+        for(int r=0; r<R; r++){
+            st = new StringTokenizer(br.readLine());
+            for(int c=0; c<C; c++){
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
         }
+
+        q = new LinkedList<>();
+
+        if(map[0][0]==2) {
+            chk[0][0][1] = true;
+            q.offer(new Pair(0,0,0, true));
+        }
+        else {
+            chk[0][0][0] = true;
+            q.offer(new Pair(0,0,0, false));
+        }
+        bfs(0,0);
     }
 }
