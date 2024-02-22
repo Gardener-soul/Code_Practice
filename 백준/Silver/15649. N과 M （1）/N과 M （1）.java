f6fa1623d25a6190c7efb5d4cce510d9ca7f2bc2
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2024-02-22 22:22:27 +0900

    [Silver III] Title: N과 M (1), Time: 2244 ms, Memory: 73364 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,49 +1,42 @@
 import java.util.Scanner;
 
 public class Main {
-	
-	static int N,M;
-	static int[] arr;
-	static int[] res;
-	static boolean[] chk;
-	
-	public static void perm(int level) {
-		
-		if(level == M) {
-			
-			for(int i=0; i<M; i++) {
-				System.out.print(res[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		for(int i=0; i<N; i++) {
-			if(chk[i]) continue;
-			chk[i] = true;
-			res[level] = arr[i];
-			perm(level+1);
-			chk[i] = false;
-		}
-		
-	}
-	
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		N = scanner.nextInt(); // 숫자의 개수
-		M = scanner.nextInt(); // 몇개 뽑을건지
-		
-		arr = new int[N];
-		res = new int[M];
-		chk = new boolean[N];
-		int cnt = 1;
-		
-		for(int i=0; i<N; i++) {
-			arr[i] = cnt++;
-		}
-		
-		perm(0);
-		
-	}
+
+    static int N,M;
+    static int[] arr, res;
+    static boolean[] chk;
+
+    public static void perm(int idx){
+        if(idx==M){
+            for(int i=0; i<M; i++){
+                System.out.print(res[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=0; i<N; i++){
+            if(chk[i]) continue;
+
+            res[idx] = arr[i];
+            chk[i] = true;
+            perm(idx+1);
+            chk[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+        arr = new int[N];
+        res = new int[M];
+        chk = new boolean[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = i+1;
+        }
+
+        perm(0);
+    }
 }
