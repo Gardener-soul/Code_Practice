ef579c99a8bce42ad484e72f973d69aba11c99c5
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2024-05-21 14:03:22 +0900

    [Bronze I] Title: 단어 공부, Time: 176 ms, Memory: 20688 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,30 +1,37 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String str = scanner.next();
-		int[] al = new int[26];
-		str = str.toUpperCase();
-		
-		for(int i=0; i<str.length(); i++) {
-			char s = str.charAt(i);
-			
-			al[s-'A']++;
-		}
-		
-		int max=-1;
-		char ch = '?';
-		for(int i=0; i<26; i++) {
-			if(max<al[i]) {
-				max = al[i];
-				ch = (char)(i + 'A');
-			} 
-			else if (al[i]==max) {
-				ch='?';
-			}
-		}
-		System.out.println(ch);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        int[] arr = new int[50];
+
+        //A: 17 , a : 49. Z: 42. z:74
+        for(int i=0; i<word.length(); i++){
+            int tmp = word.charAt(i) - '0';
+            if(tmp>48) tmp-=32;
+
+            arr[tmp]++;
+        }
+
+        int max = 0;
+        for(int i=0; i<50; i++){
+            max = Math.max(max,arr[i]);
+        }
+
+        int same = 0;
+        int ans = 0;
+        for(int i=0; i<50; i++){
+            if(max==arr[i]) {
+                same++;
+                ans = i;
+            }
+        }
+
+        char answer = (char) ((char) ans+ '0');
+        if(same>1) System.out.println('?');
+        else System.out.println(answer);
+    }
 }
