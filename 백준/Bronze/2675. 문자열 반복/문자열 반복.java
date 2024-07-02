49da4ae7853c536413c975c23f9a7cf896503b7b
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2024-07-02 15:56:28 +0900

    [Bronze II] Title: 문자열 반복, Time: 72 ms, Memory: 11460 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,25 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int repeat = Integer.parseInt(st.nextToken());
+        StringBuilder answer = new StringBuilder();
 
-	public static void main(String[] args) {
-		      
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        
-        for(int i=0; i<n; i++) {
-        	
-        	int r = scanner.nextInt();
-        	String s = scanner.next();
-        	
-        	for(int j=0; j<s.length(); j++) {
-        		for(int k=0; k<r; k++) {
-        			System.out.print(s.charAt(j));
-        		}
-        		
-        	}
-        	System.out.println();
+        for (int i = 0; i < repeat; i++) {
+            st = new StringTokenizer(br.readLine());
+            int re = Integer.parseInt(st.nextToken()); // 반복 개수
+            String word = st.nextToken();
+            for (int k = 0; k < word.length(); k++) {
+                for (int j = 0; j < re; j++) {
+                    answer.append(word.charAt(k));
+                }
+            }
+            answer.append("\n");
         }
-	}
-	
+        System.out.println(answer);
+    }
 }
