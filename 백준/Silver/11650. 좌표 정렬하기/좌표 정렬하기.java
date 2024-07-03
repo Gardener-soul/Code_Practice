0114fa6db723219dfc8298666d6f8c34a5e4b75a
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2024-07-03 21:28:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 616 ms, Memory: 48408 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -8,25 +8,25 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
         int[][] arr = new int[num][2];
 
-        for (int i = 0; i < num; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<num; i++){
+            st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
+        Arrays.sort(arr, new Comparator<int[]>(){
             @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
+            public int compare(int[] o1, int[] o2){
+                if(o1[0]==o2[0]){
                     return o1[1] - o2[1];
-                }
-                return o1[0] - o2[0];
+                } else return o1[0]-o2[0];
             }
         });
-
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < num; i++) {
             sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
