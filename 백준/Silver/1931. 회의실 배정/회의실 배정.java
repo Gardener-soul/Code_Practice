cafd1edb6a8416e906ab887f7ac9b9337d544552
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2024-07-03 20:57:26 +0900

    [Silver I] Title: 회의실 배정, Time: 444 ms, Memory: 42396 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,46 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		int[][] map = new int[N][2];
-		int arr = 0;		
-		
-		for(int i=0; i<N; i++) {
-						
-			map[i][0] = scanner.nextInt();
-			map[i][1] = scanner.nextInt();
-		}
-		
-		Arrays.sort(map, new Comparator<int[]>() {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[1]==o2[1]) {
-					return o1[0] - o2[0];
-				}
-				return o1[1]-o2[1];
-			}
-			
-		});
-		
-		int cnt = 0;
-		int end = 0;
-		
-		for(int i=0; i<N; i++) {
-			
-			if(end<=map[i][0]) {
-				end = map[i][1];
-				cnt++;
-			}
-			
-		}
-		System.out.println(cnt);
-		scanner.close();
-	}
+        int time = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[time][2];
+
+        for(int i=0; i<time; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            arr[i][0] = start;
+            arr[i][1] = end;
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1]==o2[1]) {
+                    return o1[0]-o2[0];
+                }
+
+                return o1[1]-o2[1];
+            }
+        });
+
+        int ans = 0;
+        int end = 0;
+        for(int i=0; i<time; i++){
+            if(end<=arr[i][0]){
+                end = arr[i][1];
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
 }
