a5f27c566a9cd946375f8355e43d1d519796a1de
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2025-03-19 11:18:38 +0900

    [Gold V] Title: 토마토, Time: 560 ms, Memory: 125184 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -5,97 +5,87 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Pair{
-    int h,r,c,cnt;
+class Tomato {
+    int l, c, r;
 
-    public Pair(int h, int r, int c, int cnt) {
-        this.h = h;
-        this.r = r;
+    public Tomato(int l, int c, int r) {
+        this.l = l;
         this.c = c;
-        this.cnt = cnt;
+        this.r = r;
     }
 }
 
 public class Main {
 
-    static int H,R,C;
-    static int[] deh = {-1,1,0,0,0,0};
-    static int[] der = {0,0,-1,1,0,0};
-    static int[] dec = {0,0,0,0,-1,1};
-    static int[][][] map;
-    static boolean[][][] chk;
-    static Queue<Pair> q = new LinkedList<>();
+    static int R, C, L;
+    static int[][][] arr;
+    // 방향 배열: 좌우, 위아래, 전후 (층)
+    static int[] dr = {0, 0, -1, 0, 1, 0};
+    static int[] dc = {0, 0, 0, -1, 0, 1};
+    static int[] dl = {-1, 1, 0, 0, 0, 0};
+    static Queue<Tomato> q = new LinkedList<>();
 
-    public static void bfs(){
+    public static void bfs() {
+        while (!q.isEmpty()) {
+            Tomato tmp = q.poll();
+            int tr = tmp.r;
+            int tc = tmp.c;
+            int tl = tmp.l;
 
-        while(!q.isEmpty()){
+            for (int i = 0; i < 6; i++) {
+                int der = tr + dr[i];
+                int dec = tc + dc[i];
+                int del = tl + dl[i];
 
-            Pair tmp = q.poll();
-            int nh = tmp.h;
-            int nr = tmp.r;
-            int nc = tmp.c;
-            int ncnt = tmp.cnt;
-
-            for(int d=0; d<6; d++){
-
-                int th = nh + deh[d];
-                int tr = nr + der[d];
-                int tc = nc + dec[d];
-
-                if(th<H && th>=0 && tr<R && tr>=0 && tc<C && tc>=0 && map[th][tr][tc]==0 && !chk[th][tr][tc]){
-                    chk[th][tr][tc]= true;
-                    map[th][tr][tc] = ncnt+1;
-                    q.offer(new Pair(th,tr,tc,ncnt+1));
+                // 범위 검사 및 아직 익지 않은 토마토(0)인 경우
+                if (der < R && dec < C && del < L && der >= 0 && dec >= 0 && del >= 0 && arr[del][dec][der] == 0) {
+                    // 현재 토마토의 날짜 + 1을 이웃에 기록
+                    arr[del][dec][der] = arr[tl][tc][tr] + 1;
+                    q.offer(new Tomato(del, dec, der));
                 }
             }
         }
     }
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 문제의 입력 순서: M N H (가로, 세로, 층)
         StringTokenizer st = new StringTokenizer(br.readLine());
-        C = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-        map = new int[H][R][C];
-        chk = new boolean[H][R][C];
-        int zeroCnt = 0;
-        int oneCnt = 0;
+        R = Integer.parseInt(st.nextToken()); // 가로 (열 수)
+        C = Integer.parseInt(st.nextToken()); // 세로 (행 수)
+        L = Integer.parseInt(st.nextToken()); // 층
+        arr = new int[L][C][R];
 
-        for(int h=0; h<H; h++) {
-            for (int r = 0; r < R; r++) {
+        // 각 층마다 C개의 줄, 각 줄에 R개의 숫자
+        for (int l = 0; l < L; l++) {
+            for (int c = 0; c < C; c++) {
                 st = new StringTokenizer(br.readLine());
-                for (int c = 0; c < C; c++) {
-                    map[h][r][c] = Integer.parseInt(st.nextToken());
-                    if(map[h][r][c]==0) zeroCnt++;
-                    else if(map[h][r][c]==1) {
-                        oneCnt++;
-                        chk[h][r][c]=true;
-                        q.offer(new Pair(h,r,c,0));
+                for (int r = 0; r < R; r++) {
+                    arr[l][c][r] = Integer.parseInt(st.nextToken());
+                    if (arr[l][c][r] == 1) { // 이미 익은 토마토
+                        q.offer(new Tomato(l, c, r));
                     }
                 }
             }
         }
 
-         bfs();
+        bfs();
 
-        int max = 0;
-        outer : for(int h=0; h<H; h++){
-            for(int r=0; r<R; r++){
-                for(int c=0; c<C; c++){
-                    if(map[h][r][c]==0) {
-                        max = -1;
-                        break outer;
-                    } else {
-                        max = Math.max(max,map[h][r][c]);
+        int days = 0;
+        for (int l = 0; l < L; l++) {
+            for (int c = 0; c < C; c++) {
+                for (int r = 0; r < R; r++) {
+                    if (arr[l][c][r] == 0) {
+                        // 익지 않은 토마토가 있으면
+                        System.out.println(-1);
+                        return;
                     }
+                    days = Math.max(days, arr[l][c][r]);
                 }
             }
         }
-        
-        if (zeroCnt==0) System.out.println(0);
-        else if (oneCnt==0) System.out.println(-1);
-        else System.out.println(max);
+
+        // 최초 익은 토마토가 1이므로, 걸린 날짜는 (최대값 - 1)
+        System.out.println(days - 1);
     }
 }
