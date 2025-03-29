8d26f2776d4423bab359071c1489b21c06e84477
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2025-03-29 09:28:17 +0900

    [Silver IV] Title: 수 찾기, Time: 424 ms, Memory: 52224 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,57 +1,46 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		int[] arr = new int[N];
-		
-		for(int i=0; i<N; i++) {
-			arr[i] = scanner.nextInt();
-		}
-		
-		Arrays.sort(arr);
-		
-		int M = scanner.nextInt();
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<M; i++) {
-			if(binarySearch(arr,scanner.nextInt())>=0) {
-				sb.append(1).append("\n");
-			}
-			else {
-				sb.append(0).append("\n");
-			}
-		}
-		System.out.println(sb);
-	} //main
-	
-	public static int binarySearch(int[] arr, int key) {
-		int left = 0;
-		int right = arr.length-1;
-		
-		while(left<=right) {
-			
-			int mid = (left + right) / 2;
-			
-			if(key<arr[mid]) {
-				right = mid-1;
-			}
 
-			else if (key > arr[mid]) {
-				left = mid + 1;
-			}
-			
-			else {
-				return mid;
-			}
-			
-		}
-		
-		return -1;
-		
-	}
-	
+    public static int binarySearch(int[] arr, int a) {
+
+        int lt = 0;
+        int rt = arr.length-1;
+
+        while(lt<=rt) {
+            int mid = (lt+rt) / 2;
+
+            if(arr[mid]==a) return 1;
+            else if (arr[mid]<a) lt=mid+1;
+            else rt=mid-1;
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        Arrays.sort(arr);
+        for(int i=0; i<M; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            sb.append(binarySearch(arr, tmp)).append('\n');
+        }
+
+        System.out.println(sb);
+    }
 }
